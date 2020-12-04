36f2615f32cdb42993cb8f3b751124b6094187f7
caleb mccombs <cthepyro@gmail.com>
2020-12-03 16:14:26 -0800

    Abandond the graphical approach, this build wasthe closesti have to working

--- raygame/main.cpp
+++ raygame/main.cpp
@@ -1,59 +1,150 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - basic window
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2013-2016 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include <string>
 
-#include "raylib.h"
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+char gameBoard[3][3] = {
+		{' ',' ',' '},
+		{' ',' ',' '},
+		{' ',' ',' '} };
+void drawBoard()
+{ 
+	for (int i = 0;i < 3;i++) {
+		cout << " " << gameBoard[i][0] << " | " << gameBoard[i][1] << " | " << gameBoard[i][2] << " | "<<endl;
+		if (i < 2) {
+			cout << "-------------"<<endl;
+		}
+	}
+	
+}
+void takeTurn(bool turn) 
+{
+
+	int pos = 0;
+	cin >> pos;
+	if(turn)
+	{
+		if (pos == 1 && gameBoard[0][0] == ' ')
+		{
+			gameBoard[0][0] = 'X';
+		}
+		else if (pos == 2 && gameBoard[0][1] == ' ')
+		{
+			gameBoard[0][1] = 'X';
+		}
+		else if (pos == 3 && gameBoard[0][2] == ' ')
+		{
+			gameBoard[0][2] = 'X';
+		}
+		else if (pos == 4 && gameBoard[1][0] == ' ')
+		{
+			gameBoard[1][0] = 'X';
+		}
+		else if (pos == 5 && gameBoard[1][1] == ' ')
+		{
+			gameBoard[1][1] = 'X';
+		}
+		else if (pos == 6 && gameBoard[1][2] == ' ')
+		{
+			gameBoard[1][2] = 'X';
+		}
+		else if (pos == 7 && gameBoard[2][0] == ' ') {
+			gameBoard[2][0] = 'X';
+		}
+		else if (pos == 8 && gameBoard[2][1] == ' ') {
+			gameBoard[2][1] = 'X';
+		}
+		else if (pos == 9 && gameBoard[2][2] == ' ')
+		{
+			gameBoard[2][2] = 'X';
+		}
+		else 
+		{
+			cout << "not a valid input" << endl;
+			pos = 0;
+		}
+	}
+	else {
+		if (pos == 1 && gameBoard[0][0])
+		{
+			gameBoard[0][0] = '0';
+		}
+		else if (pos == 2 && gameBoard[0][1])
+		{
+			gameBoard[0][1] = '0';
+		}
+		else if (pos == 3 && gameBoard[0][2])
+		{
+			gameBoard[0][2] = '0';
+		}
+		else if (pos == 4 && gameBoard[1][0])
+		{
+			gameBoard[1][0] = '0';
+		}
+		else if (pos == 5 && gameBoard[1][1])
+		{
+			gameBoard[1][1] = '0';
+		}
+		else if (pos == 6 && gameBoard[1][2])
+		{
+			gameBoard[1][2] = '0';
+		}
+		else if (pos == 7 && gameBoard[2][0]) {
+			gameBoard[2][0] = '0';
+		}
+		else if (pos == 8 && gameBoard[2][1]) {
+			gameBoard[2][1] = '0';
+		}
+		else if (pos == 9 && gameBoard[2][2])
+		{
+			gameBoard[2][2] = '0';
+		}
+		else
+		{
+			cout << "not a valid input" << endl;
+			pos = 0;
+		}
+	}
+}
+char testWin()
+{
+	char win = '0';
+	if      (gameBoard[0][0] == 'X' && gameBoard[0][1] == 'X' && gameBoard[0][2] == 'X') { win = '1'; }
+	else if (gameBoard[1][0] == 'X' && gameBoard[1][1] == 'X' && gameBoard[1][2] == 'X') { win = '1'; }
+	else if (gameBoard[2][0] == 'X' && gameBoard[2][1] == 'X' && gameBoard[2][2] == 'X') { win = '1'; }
+	else if (gameBoard[0][0] == 'X' && gameBoard[1][0] == 'X' && gameBoard[2][0] == 'X') { win = '1'; }
+	else if (gameBoard[0][1] == 'X' && gameBoard[1][1] == 'X' && gameBoard[2][1] == 'X') { win = '1'; }
+	else if (gameBoard[0][2] == 'X' && gameBoard[1][2] == 'X' && gameBoard[2][2] == 'X') { win = '1'; }
+	else if (gameBoard[1][1] == 'X' && gameBoard[2][2] == 'X' && gameBoard[0][0] == 'X') { win = '1'; }
+	else if (gameBoard[2][0] == 'X' && gameBoard[2][2] == 'X' && gameBoard[0][2] == 'X') { win = '1'; }
+
+	if      (gameBoard[0][0] == '0' && gameBoard[0][1] == '0' && gameBoard[0][2] == '0') { win = '2'; }
+	else if (gameBoard[1][0] == '0' && gameBoard[1][1] == '0' && gameBoard[1][2] == '0') { win = '2'; }
+	else if (gameBoard[2][0] == '0' && gameBoard[2][1] == '0' && gameBoard[2][2] == '0') { win = '2'; }
+	else if (gameBoard[0][0] == '0' && gameBoard[1][0] == '0' && gameBoard[2][0] == '0') { win = '2'; }
+	else if (gameBoard[0][1] == '0' && gameBoard[1][1] == '0' && gameBoard[2][1] == '0') { win = '2'; }
+	else if (gameBoard[0][2] == '0' && gameBoard[1][2] == '0' && gameBoard[2][2] == '0') { win = '2'; }
+	else if (gameBoard[1][1] == '0' && gameBoard[2][2] == '0' && gameBoard[0][0] == '0') { win = '2'; }
+	else if (gameBoard[2][0] == '0' && gameBoard[2][2] == '0' && gameBoard[0][2] == '0') { win = '2'; }
+	return win;
+
+}
 
 int main()
 {
-	// Initialization
-	//--------------------------------------------------------------------------------------
-	int screenWidth = 800;
-	int screenHeight = 450;
-
-	InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-	SetTargetFPS(60);
-
-	//initial game board data
-	char gameBoard[3][3] = { {'*', '*', '*'},
-							  {'*', '*', '*'},
-							  {'*', '*', '*'} };
-	bool p1Wins = false;
-	bool p2Wins = false;
-	//--------------------------------------------------------------------------------------
-
-	// Main game loop
-	while (!WindowShouldClose())    // Detect window close button or ESC key
-	{
-		// Update
-		//----------------------------------------------------------------------------------
-		// TODO: Update your variables here
-		//----------------------------------------------------------------------------------
-
-		// Draw
-		//----------------------------------------------------------------------------------
-		BeginDrawing();
-
-		ClearBackground(RAYWHITE);
-
-		;
-
-		EndDrawing();
-		//----------------------------------------------------------------------------------
+	bool turn = true;
+	bool playing = true;
+	while (playing) {
+		drawBoard();
+		takeTurn(turn);
+		turn = !turn;
+		testWin();
 	}
-
-	// De-Initialization
-	//--------------------------------------------------------------------------------------   
-	CloseWindow();        // Close window and OpenGL context
-	//--------------------------------------------------------------------------------------
-
+	
 	return 0;
 }
